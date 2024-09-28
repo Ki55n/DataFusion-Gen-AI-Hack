842a5078b042aefddea80e1a315645ad26a02a2d
Himesh Parashar <himeshparashar424@gmail.com>
2024-09-29 00:31:28 +0530

    google icon fix

--- Interface/src/app/login/page.tsx
+++ Interface/src/app/login/page.tsx
@@ -125,29 +125,13 @@ export default function Login() {
           onClick={handleSignInWithGoogle}
           className="flex items-center justify-center w-full p-3 bg-gray-700 text-white rounded-lg hover:bg-gray-600 transition shadow-md"
         >
-          <svg
-            className="w-5 h-5 mr-2"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M22.674 11.24c0-.748-.068-1.464-.187-2.154H12v4.07h5.961c-.257 1.29-.998 2.381-2.112 3.109v2.566h3.418c2.006-1.85 3.156-4.569 3.156-7.591z"
-              fill="#4285F4"
-            />
-            <path
-              d="M12 23c3.106 0 5.705-1.034 7.606-2.806l-3.418-2.566c-.948.635-2.145 1.015-3.569 1.015-2.749 0-5.073-1.856-5.906-4.348H4.2v2.735C6.089 20.975 8.848 23 12 23z"
-              fill="#34A853"
-            />
-            <path
-              d="M6.094 14.295a6.95 6.95 0 01-.365-2.295c0-.796.133-1.565.365-2.295V6.97H4.2a10.972 10.972 0 000 9.86l1.894-2.535z"
-              fill="#FBBC05"
-            />
-            <path
-              d="M12 4.856c1.512 0 2.868.52 3.937 1.542l2.936-2.936C16.59 1.977 14.284 1 12 1 8.848 1 6.089 3.025 4.2 6.971l1.894 2.735c.834-2.492 3.158-4.348 5.906-4.348z"
-              fill="#EA4335"
-            />
-          </svg>
+          <Image
+            alt="Google"
+            src={"/google-logo.svg"}
+            height={20}
+            width={20}
+            className="mr-3"
+          />
           Sign in with Google
         </button>
 
